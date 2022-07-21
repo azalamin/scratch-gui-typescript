8ea111aa55493eadb964514d4debc7e10c6d335c
azalamins <azalamins@gmail.com>
2022-07-21 16:01:24 +0600

    updated to typescript

--- front/src/containers/stage-header.tsx
+++ front/src/containers/stage-header.tsx
@@ -1,64 +1,65 @@
-import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
-import bindAll from 'lodash.bindall';
-import VM from 'scratch-vm';
-import {STAGE_SIZE_MODES} from '../lib/layout-constants';
-import {setStageSize} from '../reducers/stage-size';
-import {setFullScreen} from '../reducers/mode';
+import { useCallback, useEffect } from 'react';
+import { STAGE_SIZE_MODES } from '../lib/layout-constants';
+import { setFullScreen } from '../reducers/mode';
+import { setStageSize } from '../reducers/stage-size';
 
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import StageHeaderComponent from '../components/stage-header/stage-header.jsx';
 
-const StageHeader = (prop) => {
-    useEffect(() => {
-      document.addEventListener('keydown', handleKeyPress);
-      return () => {
-       document.removeEventListener('keydown', handleKeyPress);
+const StageHeader = (prop: PropsInterface) => {
+  const handleKeyPress = useCallback(
+    event => {
+      if (event.key === 'Escape' && prop.isFullScreen) {
+        prop.onSetStageUnFull(false);
       }
-    }, []);
+    },
+    [prop]
+  );
 
-     const handleKeyPress = (event) => {
-        if (event.key === 'Escape' && prop.isFullScreen) {
-            prop.onSetStageUnFull(false);
-        }
-    }
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress);
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [handleKeyPress]);
 
-    const {...props} = prop;
-    
-    return (
-        <StageHeaderComponent
-            {...props}
-            onKeyPress={handleKeyPress}
-        />
-    );
+  const { ...props } = prop;
+
+  return <StageHeaderComponent {...props} onKeyPress={handleKeyPress} />;
 };
 
+interface PropsInterface {
+  isFullScreen: boolean;
+  isPlayerOnly: boolean;
+  onSetStageUnFull: any;
+  showBranding: boolean;
+  stageSizeMode: any;
+  vm: any;
+}
 
-StageHeader.propTypes = {
-    isFullScreen: PropTypes.bool,
-    isPlayerOnly: PropTypes.bool,
-    onSetStageUnFull: PropTypes.func.isRequired,
-    showBranding: PropTypes.bool,
-    stageSizeMode: PropTypes.oneOf(Object.keys(STAGE_SIZE_MODES)),
-    vm: PropTypes.instanceOf(VM).isRequired
-};
+// TODO
+// StageHeader.propTypes = {
+//     isFullScreen: PropTypes.bool,
+//     isPlayerOnly: PropTypes.bool,
+//     onSetStageUnFull: PropTypes.func.isRequired,
+//     showBranding: PropTypes.bool,
+//     stageSizeMode: PropTypes.oneOf(Object.keys(STAGE_SIZE_MODES)),
+//     vm: PropTypes.instanceOf(VM).isRequired
+// };
 
-const mapStateToProps = state => ({
-    stageSizeMode: state.scratchGui.stageSize.stageSize,
-    showBranding: state.scratchGui.mode.showBranding,
-    isFullScreen: state.scratchGui.mode.isFullScreen,
-    isPlayerOnly: state.scratchGui.mode.isPlayerOnly
+const mapStateToProps = (state: any) => ({
+  stageSizeMode: state.scratchGui.stageSize.stageSize,
+  showBranding: state.scratchGui.mode.showBranding,
+  isFullScreen: state.scratchGui.mode.isFullScreen,
+  isPlayerOnly: state.scratchGui.mode.isPlayerOnly,
 });
 
-const mapDispatchToProps = dispatch => ({
-    onSetStageLarge: () => dispatch(setStageSize(STAGE_SIZE_MODES.large)),
-    onSetStageSmall: () => dispatch(setStageSize(STAGE_SIZE_MODES.small)),
-    onSetStageFull: () => dispatch(setFullScreen(true)),
-    onSetStageUnFull: () => dispatch(setFullScreen(false))
+const mapDispatchToProps = (dispatch: any) => ({
+  onSetStageLarge: () => dispatch(setStageSize(STAGE_SIZE_MODES.large)),
+  onSetStageSmall: () => dispatch(setStageSize(STAGE_SIZE_MODES.small)),
+  onSetStageFull: () => dispatch(setFullScreen(true)),
+  onSetStageUnFull: () => dispatch(setFullScreen(false)),
 });
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(StageHeader);
+export default connect(mapStateToProps, mapDispatchToProps)(StageHeader);
--- front/src/containers/stage-selector.tsx
+++ front/src/containers/stage-selector.tsx
@@ -1,204 +1,256 @@
-import bindAll from 'lodash.bindall';
 import omit from 'lodash.omit';
-import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
-import {intlShape, injectIntl} from 'react-intl';
+import { useCallback, useEffect } from 'react';
+import { injectIntl, IntlShape } from 'react-intl';
 
-import {connect} from 'react-redux';
-import {openBackdropLibrary} from '../reducers/modals';
-import {activateTab, COSTUMES_TAB_INDEX} from '../reducers/editor-tab';
-import {showStandardAlert, closeAlertWithId} from '../reducers/alerts';
-import {setHoveredSprite} from '../reducers/hovered-target';
+import { connect } from 'react-redux';
+import { fetchCode } from '../lib/backpack-api';
 import DragConstants from '../lib/drag-constants';
 import DropAreaHOC from '../lib/drop-area-hoc.jsx';
-import ThrottledPropertyHOC from '../lib/throttled-property-hoc.jsx';
-import {emptyCostume} from '../lib/empty-assets';
+import { emptyCostume } from '../lib/empty-assets';
 import sharedMessages from '../lib/shared-messages';
-import {fetchCode} from '../lib/backpack-api';
-import {getEventXY} from '../lib/touch-utils';
+import ThrottledPropertyHOC from '../lib/throttled-property-hoc.jsx';
+import { getEventXY } from '../lib/touch-utils';
+import { closeAlertWithId, showStandardAlert } from '../reducers/alerts';
+import { activateTab, COSTUMES_TAB_INDEX } from '../reducers/editor-tab';
+import { setHoveredSprite } from '../reducers/hovered-target';
+import { openBackdropLibrary } from '../reducers/modals';
 
 import StageSelectorComponent from '../components/stage-selector/stage-selector.jsx';
 
+import { costumeUpload, handleFileUpload } from '../lib/file-uploader.js';
 import backdropLibraryContent from '../lib/libraries/backdrops.json';
-import {handleFileUpload, costumeUpload} from '../lib/file-uploader.js';
 
 const dragTypes = [
-    DragConstants.COSTUME,
-    DragConstants.SOUND,
-    DragConstants.BACKPACK_COSTUME,
-    DragConstants.BACKPACK_SOUND,
-    DragConstants.BACKPACK_CODE
+  DragConstants.COSTUME,
+  DragConstants.SOUND,
+  DragConstants.BACKPACK_COSTUME,
+  DragConstants.BACKPACK_SOUND,
+  DragConstants.BACKPACK_CODE,
 ];
 
 const DroppableThrottledStage = DropAreaHOC(dragTypes)(
-    ThrottledPropertyHOC('url', 500)(StageSelectorComponent)
+  ThrottledPropertyHOC('url', 500)(StageSelectorComponent)
 );
 
-const StageSelector = (props) => {
-    useEffect(() => {
-      document.addEventListener('touchend', handleTouchEnd);
+const StageSelector = (props: PropsInterface) => {
+  let fileInput: any;
+  let ref: any;
+  const addBackdropFromLibraryItem = (item: any, shouldActivateTab = true) => {
+    const vmBackdrop = {
+      name: item.name,
+      md5: item.md5ext,
+      rotationCenterX: item.rotationCenterX,
+      rotationCenterY: item.rotationCenterY,
+      bitmapResolution: item.bitmapResolution,
+      skinId: null,
+    };
+    handleNewBackdrop(vmBackdrop, shouldActivateTab);
+  };
 
-      return () => {
-        document.removeEventListener('touchend', handleTouchEnd);
+  const handleClick = () => {
+    props.onSelect(props.id);
+  };
+  const handleNewBackdrop = (backdrops_: any, shouldActivateTab = true) => {
+    const backdrops = Array.isArray(backdrops_) ? backdrops_ : [backdrops_];
+    return Promise.all(
+      backdrops.map(backdrop => props.vm.addBackdrop(backdrop.md5, backdrop))
+    ).then(() => {
+      if (shouldActivateTab) {
+        return props.onActivateTab(COSTUMES_TAB_INDEX);
       }
-    }, [handleTouchEnd]);
-
-    const handleTouchEnd = (e) => {
-        const {x, y} = getEventXY(e);
-        const {top, left, bottom, right} =  ref.getBoundingClientRect();
-        if (x >= left && x <= right && y >= top && y <= bottom) {
-             handleMouseEnter();
-        }
-    }
-    const addBackdropFromLibraryItem = (item, shouldActivateTab = true) => {
-        const vmBackdrop = {
-            name: item.name,
-            md5: item.md5ext,
-            rotationCenterX: item.rotationCenterX,
-            rotationCenterY: item.rotationCenterY,
-            bitmapResolution: item.bitmapResolution,
-            skinId: null
-        };
-         handleNewBackdrop(vmBackdrop, shouldActivateTab);
-    }
-    const handleClick = () => {
-         props.onSelect( props.id);
-    }
-    const handleNewBackdrop = (backdrops_, shouldActivateTab = true) => {
-        const backdrops = Array.isArray(backdrops_) ? backdrops_ : [backdrops_];
-        return Promise.all(backdrops.map(backdrop =>
-             props.vm.addBackdrop(backdrop.md5, backdrop)
-        )).then(() => {
-            if (shouldActivateTab) {
-                return  props.onActivateTab(COSTUMES_TAB_INDEX);
-            }
-        });
-    }
-    const handleSurpriseBackdrop = (e) => {
-        e.stopPropagation(); // Prevent click from falling through to selecting stage.
-        // @todo should this not add a backdrop you already have?
-        const item = backdropLibraryContent[Math.floor(Math.random() * backdropLibraryContent.length)];
-         addBackdropFromLibraryItem(item, false);
-    }
-    const handleEmptyBackdrop = (e) => {
-        e.stopPropagation(); // Prevent click from falling through to stage selector, select it manually below
-         props.vm.setEditingTarget( props.id);
-         handleNewBackdrop(emptyCostume( props.intl.formatMessage(sharedMessages.backdrop, {index: 1})));
-    }
-    const handleBackdropUpload = (e) => {
-        const storage =  props.vm.runtime.storage;
-         props.onShowImporting();
-        handleFileUpload(e.target, (buffer, fileType, fileName, fileIndex, fileCount) => {
-            costumeUpload(buffer, fileType, storage, vmCostumes => {
-                 props.vm.setEditingTarget( props.id);
-                vmCostumes.forEach((costume, i) => {
-                    costume.name = `${fileName}${i ? i + 1 : ''}`;
-                });
-                 handleNewBackdrop(vmCostumes).then(() => {
-                    if (fileIndex === fileCount - 1) {
-                         props.onCloseImporting();
-                    }
-                });
-            },  props.onCloseImporting);
-        },  props.onCloseImporting);
-    }
-    const handleFileUploadClick = (e) => {
-        e.stopPropagation(); // Prevent click from selecting the stage, that is handled manually in backdrop upload
-         fileInput.click();
-    }
-    const handleMouseEnter = () => {
-         props.dispatchSetHoveredSprite( props.id);
-    }
-    const handleMouseLeave = () => {
-         props.dispatchSetHoveredSprite(null);
-    }
-    const handleDrop = (dragInfo) => {
-        if (dragInfo.dragType === DragConstants.COSTUME) {
-             props.vm.shareCostumeToTarget(dragInfo.index,  props.id);
-        } else if (dragInfo.dragType === DragConstants.SOUND) {
-             props.vm.shareSoundToTarget(dragInfo.index,  props.id);
-        } else if (dragInfo.dragType === DragConstants.BACKPACK_COSTUME) {
-             props.vm.addCostume(dragInfo.payload.body, {
-                name: dragInfo.payload.name
-            },  props.id);
-        } else if (dragInfo.dragType === DragConstants.BACKPACK_SOUND) {
-             props.vm.addSound({
-                md5: dragInfo.payload.body,
-                name: dragInfo.payload.name
-            },  props.id);
-        } else if (dragInfo.dragType === DragConstants.BACKPACK_CODE) {
-            fetchCode(dragInfo.payload.bodyUrl)
-                .then(blocks => {
-                     props.vm.shareBlocksToTarget(blocks,  props.id);
-                     props.vm.refreshWorkspace();
-                });
-        }
-    }
-    let fileInput;
-    const setFileInput = (input) => {
-         fileInput = input;
-    }
-    let ref;
-    const setRef = (ref) => {
-         ref = ref;
-    }
-
-    const componentProps = omit( props, [
-            'asset', 'dispatchSetHoveredSprite', 'id', 'intl',
-            'onActivateTab', 'onSelect', 'onShowImporting', 'onCloseImporting']
+    });
+  };
+  const handleSurpriseBackdrop = (e: any) => {
+    e.stopPropagation(); // Prevent click from falling through to selecting stage.
+    // @todo should this not add a backdrop you already have?
+    const item =
+      backdropLibraryContent[
+        Math.floor(Math.random() * backdropLibraryContent.length)
+      ];
+    addBackdropFromLibraryItem(item, false);
+  };
+  const handleEmptyBackdrop = (e: any) => {
+    e.stopPropagation(); // Prevent click from falling through to stage selector, select it manually below
+    props.vm.setEditingTarget(props.id);
+    handleNewBackdrop(
+      emptyCostume(
+        props.intl.formatMessage(sharedMessages.backdrop, { index: 1 })
+      )
     );
-    
-    return (
-        <DroppableThrottledStage
-            componentRef={ setRef}
-            fileInputRef={ setFileInput}
-            onBackdropFileUpload={ handleBackdropUpload}
-            onBackdropFileUploadClick={ handleFileUploadClick}
-            onClick={ handleClick}
-            onDrop={ handleDrop}
-            onEmptyBackdropClick={ handleEmptyBackdrop}
-            onMouseEnter={ handleMouseEnter}
-            onMouseLeave={ handleMouseLeave}
-            onSurpriseBackdropClick={ handleSurpriseBackdrop}
-            {...componentProps}
-        />
+  };
+  const handleBackdropUpload = (e: any) => {
+    const storage = props.vm.runtime.storage;
+    props.onShowImporting();
+    handleFileUpload(
+      e.target,
+      (
+        buffer: any,
+        fileType: any,
+        fileName: any,
+        fileIndex: any,
+        fileCount: any
+      ) => {
+        costumeUpload(
+          buffer,
+          fileType,
+          storage,
+          (vmCostumes: any) => {
+            props.vm.setEditingTarget(props.id);
+            vmCostumes.forEach((costume: any, i: any) => {
+              costume.name = `${fileName}${i ? i + 1 : ''}`;
+            });
+            handleNewBackdrop(vmCostumes).then(() => {
+              if (fileIndex === fileCount - 1) {
+                props.onCloseImporting();
+              }
+            });
+          },
+          props.onCloseImporting
+        );
+      },
+      props.onCloseImporting
     );
+  };
+  const handleFileUploadClick = (e: any) => {
+    e.stopPropagation(); // Prevent click from selecting the stage, that is handled manually in backdrop upload
+    fileInput.click();
+  };
+  const handleMouseEnter = useCallback(() => {
+    props.dispatchSetHoveredSprite(props.id);
+  }, [props]);
+
+  const handleTouchEnd = useCallback(
+    e => {
+      const { x, y } = getEventXY(e);
+      const { top, left, bottom, right } = ref.getBoundingClientRect();
+      if (x >= left && x <= right && y >= top && y <= bottom) {
+        handleMouseEnter();
+      }
+    },
+    [handleMouseEnter, ref]
+  );
+
+  const handleMouseLeave = () => {
+    props.dispatchSetHoveredSprite(null);
+  };
+  const handleDrop = (dragInfo: any) => {
+    if (dragInfo.dragType === DragConstants.COSTUME) {
+      props.vm.shareCostumeToTarget(dragInfo.index, props.id);
+    } else if (dragInfo.dragType === DragConstants.SOUND) {
+      props.vm.shareSoundToTarget(dragInfo.index, props.id);
+    } else if (dragInfo.dragType === DragConstants.BACKPACK_COSTUME) {
+      props.vm.addCostume(
+        dragInfo.payload.body,
+        {
+          name: dragInfo.payload.name,
+        },
+        props.id
+      );
+    } else if (dragInfo.dragType === DragConstants.BACKPACK_SOUND) {
+      props.vm.addSound(
+        {
+          md5: dragInfo.payload.body,
+          name: dragInfo.payload.name,
+        },
+        props.id
+      );
+    } else if (dragInfo.dragType === DragConstants.BACKPACK_CODE) {
+      fetchCode(dragInfo.payload.bodyUrl).then((blocks: any) => {
+        props.vm.shareBlocksToTarget(blocks, props.id);
+        props.vm.refreshWorkspace();
+      });
+    }
+  };
+
+  const setFileInput = (input: any) => {
+    fileInput = input;
+  };
+
+  const setRef = (refs: any) => {
+    ref = refs;
+  };
+
+  useEffect(() => {
+    document.addEventListener('touchend', handleTouchEnd);
+
+    return () => {
+      document.removeEventListener('touchend', handleTouchEnd);
+    };
+  }, [handleTouchEnd]);
+
+  const componentProps = omit(props, [
+    'asset',
+    'dispatchSetHoveredSprite',
+    'id',
+    'intl',
+    'onActivateTab',
+    'onSelect',
+    'onShowImporting',
+    'onCloseImporting',
+  ]);
+
+  return (
+    <DroppableThrottledStage
+      componentRef={setRef}
+      fileInputRef={setFileInput}
+      onBackdropFileUpload={handleBackdropUpload}
+      onBackdropFileUploadClick={handleFileUploadClick}
+      onClick={handleClick}
+      onDrop={handleDrop}
+      onEmptyBackdropClick={handleEmptyBackdrop}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onSurpriseBackdropClick={handleSurpriseBackdrop}
+      {...componentProps}
+    />
+  );
 };
 
+interface PropsInterface {
+  id: string;
+  intl: IntlShape;
+  onCloseImporting: any;
+  onSelect: any;
+  onShowImporting: any;
+  vm: any;
+  onActivateTab: any;
+  dispatchSetHoveredSprite: any;
+}
 
-StageSelector.propTypes = {
-    ...StageSelectorComponent.propTypes,
-    id: PropTypes.string,
-    intl: intlShape.isRequired,
-    onCloseImporting: PropTypes.func,
-    onSelect: PropTypes.func,
-    onShowImporting: PropTypes.func
-};
+// TODO
+// StageSelector.propTypes = {
+//     ...StageSelectorComponent.propTypes,
+//     id: PropTypes.string,
+//     intl: intlShape.isRequired,
+//     onCloseImporting: PropTypes.func,
+//     onSelect: PropTypes.func,
+//     onShowImporting: PropTypes.func
+// };
 
-const mapStateToProps = (state, {asset, id}) => ({
-    url: asset && asset.encodeDataURI(),
-    vm: state.scratchGui.vm,
-    receivedBlocks: state.scratchGui.hoveredTarget.receivedBlocks &&
-            state.scratchGui.hoveredTarget.sprite === id,
-    raised: state.scratchGui.blockDrag
+const mapStateToProps = (state: any, { asset, id }: any) => ({
+  url: asset && asset.encodeDataURI(),
+  vm: state.scratchGui.vm,
+  receivedBlocks:
+    state.scratchGui.hoveredTarget.receivedBlocks &&
+    state.scratchGui.hoveredTarget.sprite === id,
+  raised: state.scratchGui.blockDrag,
 });
 
-const mapDispatchToProps = dispatch => ({
-    onNewBackdropClick: e => {
-        e.stopPropagation();
-        dispatch(openBackdropLibrary());
-    },
-    onActivateTab: tabIndex => {
-        dispatch(activateTab(tabIndex));
-    },
-    dispatchSetHoveredSprite: spriteId => {
-        dispatch(setHoveredSprite(spriteId));
-    },
-    onCloseImporting: () => dispatch(closeAlertWithId('importingAsset')),
-    onShowImporting: () => dispatch(showStandardAlert('importingAsset'))
+const mapDispatchToProps = (dispatch: any) => ({
+  onNewBackdropClick: (e: any) => {
+    e.stopPropagation();
+    dispatch(openBackdropLibrary());
+  },
+  onActivateTab: (tabIndex: any) => {
+    dispatch(activateTab(tabIndex));
+  },
+  dispatchSetHoveredSprite: (spriteId: any) => {
+    dispatch(setHoveredSprite(spriteId));
+  },
+  onCloseImporting: () => dispatch(closeAlertWithId('importingAsset')),
+  onShowImporting: () => dispatch(showStandardAlert('importingAsset')),
 });
 
-export default injectIntl(connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(StageSelector));
+export default injectIntl(
+  connect(mapStateToProps, mapDispatchToProps)(StageSelector)
+);
