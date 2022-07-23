9debbbdd4c4605dd3b73026aba9427a2241931c6
azalamins <azalamins@gmail.com>
2022-07-23 23:18:12 +0600

    updated to tsx

---
+++ front/src/components/menu-bar/author-info.tsx
@@ -0,0 +1,52 @@
+import classNames from 'classnames';
+import { FormattedMessage, injectIntl } from 'react-intl';
+import UserAvatar from './user-avatar.jsx';
+
+import styles from './authorInfo.module.css';
+
+const AuthorInfo: any = ({
+  className,
+  imageUrl,
+  projectTitle,
+  // TODO: use userId to link to user's profile
+  userId,
+  username,
+}: PropsInterface) => (
+  <div className={classNames(className, styles.authorInfo)}>
+    <UserAvatar className={styles.avatar} imageUrl={imageUrl} />
+    <div className={styles.titleAuthor}>
+      <span className={styles.projectTitle}>{projectTitle}</span>
+      <div>
+        <span className={styles.usernameLine}>
+          <FormattedMessage
+            defaultMessage='by {username}'
+            description='Shows that a project was created by this user'
+            id='gui.authorInfo.byUser'
+            values={{
+              username: <span className={styles.username}>{username}</span>,
+            }}
+          />
+        </span>
+      </div>
+    </div>
+  </div>
+);
+
+interface PropsInterface {
+  className: string;
+  imageUrl: string;
+  projectTitle: string;
+  userId: string | boolean;
+  username: string | boolean;
+}
+
+// TODO
+// AuthorInfo.propTypes = {
+//     className: PropTypes.string,
+//     imageUrl: PropTypes.string,
+//     projectTitle: PropTypes.string,
+//     userId: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+//     username: PropTypes.oneOfType([PropTypes.string, PropTypes.bool])
+// };
+
+export default injectIntl(AuthorInfo);
