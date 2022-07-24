fdbad6de563d062bdd40e3f0a8ca44fca567ae5c
azalamins <azalamins@gmail.com>
2022-07-24 10:43:19 +0600

    js to ts updated

---
+++ front/src/lib/libraries/sprite-tags.ts
@@ -0,0 +1,13 @@
+import messages from './tag-messages.js';
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  { tag: 'animals', intlLabel: messages.animals },
+  { tag: 'people', intlLabel: messages.people },
+  { tag: 'fantasy', intlLabel: messages.fantasy },
+  { tag: 'dance', intlLabel: messages.dance },
+  { tag: 'music', intlLabel: messages.music },
+  { tag: 'sports', intlLabel: messages.sports },
+  { tag: 'food', intlLabel: messages.food },
+  { tag: 'fashion', intlLabel: messages.fashion },
+  { tag: 'letters', intlLabel: messages.letters },
+];
---
+++ front/src/lib/libraries/tutorial-tags.ts
@@ -0,0 +1,9 @@
+import messages from './tag-messages.js';
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  { tag: 'animation', intlLabel: messages.animation },
+  { tag: 'art', intlLabel: messages.art },
+  { tag: 'music', intlLabel: messages.music },
+  { tag: 'games', intlLabel: messages.games },
+  { tag: 'stories', intlLabel: messages.stories },
+];
