4d27bb2cd37ae2d2a2a89b642b1b921cbb46b476
azalamins <azalamins@gmail.com>
2022-07-24 10:54:45 +0600

    js to ts updated

--- front/src/types/index.d.ts
+++ front/src/types/index.d.ts
@@ -7,6 +7,8 @@ declare global {
     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
     webkitAudioContext: any;
     webkitOfflineAudioContext: any;
+    GA_ID: any;
+    ga: any;
   }
 
   interface Document {
