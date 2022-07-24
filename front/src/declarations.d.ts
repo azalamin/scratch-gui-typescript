fa580304b713e7d824ed42fa8d47c1e130941832
azalamins <azalamins@gmail.com>
2022-07-24 13:02:03 +0600

    js to ts updated

--- front/src/declarations.d.ts
+++ front/src/declarations.d.ts
@@ -11,3 +11,4 @@ declare module '!arraybuffer-loader!./83a9787d4cb6f3b7632b4ddfebf74367.wav';
 declare module '!arraybuffer-loader!./83c36d806dc92327b9e7049a565c6bff.wav';
 declare module 'scratch-vm/src/extension-support/argument-type';
 declare module 'scratch-vm/src/extension-support/block-type';
+declare module 'scratch-svg-renderer';
