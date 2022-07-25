72f6bc39b49e5a295d88c51579143704dd15f602
Prashant Choudhary <prashant@simplem.in>
2022-07-25 14:45:07 +0530

    minor version changes

--- front/src/components/context-menu/context-menu.tsx
+++ front/src/components/context-menu/context-menu.tsx
@@ -3,39 +3,33 @@ import { ContextMenu, MenuItem } from 'react-contextmenu';
 
 import styles from './contextMenu.module.css';
 
-const StyledContextMenu = (props: any) => (
-  <ContextMenu {...props} className={styles.contextMenu} />
-);
+const StyledContextMenu = (props: any) => <ContextMenu {...props} className={styles.contextMenu} />;
 
 const StyledMenuItem = (props: any) => (
-  <MenuItem {...props} attributes={{ className: styles.menuItem }} />
+	<MenuItem {...props} attributes={{ className: styles.menuItem }} />
 );
 
 const BorderedMenuItem = (props: any) => (
-  <MenuItem
-    {...props}
-    attributes={{
-      className: classNames(styles.menuItem, styles.menuItemBordered),
-    }}
-  />
+	<MenuItem
+		{...props}
+		attributes={{
+			className: classNames(styles.menuItem, styles.menuItemBordered),
+		}}
+	/>
 );
 
 const DangerousMenuItem = (props: any) => (
-  <MenuItem
-    {...props}
-    attributes={{
-      className: classNames(
-        styles.menuItem,
-        styles.menuItemBordered,
-        styles.menuItemDanger
-      ),
-    }}
-  />
+	<MenuItem
+		{...props}
+		attributes={{
+			className: classNames(styles.menuItem, styles.menuItemBordered, styles.menuItemDanger),
+		}}
+	/>
 );
 
 export {
-  BorderedMenuItem,
-  DangerousMenuItem,
-  StyledContextMenu as ContextMenu,
-  StyledMenuItem as MenuItem,
+	BorderedMenuItem,
+	DangerousMenuItem,
+	StyledContextMenu as ContextMenu,
+	StyledMenuItem as MenuItem,
 };
